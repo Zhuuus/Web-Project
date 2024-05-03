9901b35058280189d6d6217f36bcd71dd5494b98
Nurzat <n_turganbek@kbtu.kz>
2024-05-03 16:07:16 +0600

    add

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,12 +1,27 @@
+
 import { Component } from '@angular/core';
+import { PersonalDataService } from '../personal-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  userData: any;
 
+  constructor(private personalDataService: PersonalDataService) {}
+
+    ngOnInit(): void {
+      this.personalDataService.getUserData().subscribe(data => {
+        this.userData = data;
+      },
+      (error) => {
+        console.error('Error fetching user data:', error);
+      }
+    );
+    }
 }
--- front/src/app/personal-data.service.ts
+++ front/src/app/personal-data.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PersonalData } from './models';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,30 @@ export class PersonalDataService {
 
   getUserData(): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}user_data/`);
+  }
+  // updateUserData(userData: PersonalData): Observable<PersonalData> {
+  //   return this.http.put<PersonalData>(`${this.apiUrl}personaldataset/`, userData);
+  // }
+
+  // createPersonalData(phone_number: string): Observable<PersonalData> {
+  //   return this.http.post<PersonalData>(
+  //     `${this.apiUrl}personaldataset/`,
+  //     {phone_number}
+  //   )
+  // }
+
+  createPersonalData(formData: FormData): Observable<PersonalData> {
+    const headers = new HttpHeaders();
+    // Установите заголовки, если они требуются вашим сервером
+    // headers.append('Content-Type', 'multipart/form-data');
+    // headers.append('Accept', 'application/json');
+
+    return this.http.post<PersonalData>(`${this.apiUrl}personaldataset/`, formData, { headers });
+  }
+
+  getPersonalData(): Observable<PersonalData[]> {
+    return this.http.get<PersonalData[]>(
+      `${this.apiUrl}personaldataset/`
+    )
   }
 }

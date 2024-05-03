73f75430d9a44a3a35b7eeb3e72e45dc12d8d669
Nurzat <n_turganbek@kbtu.kz>
2024-05-03 10:06:50 +0600

    project

--- front/src/app/personal-data.service.ts
+++ front/src/app/personal-data.service.ts
@@ -7,24 +7,11 @@ import { PersonalData } from './models';
   providedIn: 'root'
 })
 export class PersonalDataService {
-  private apiUrl = 'http://localhost:8000/api/personaldataset/';
+  private apiUrl = 'http://localhost:8000/api/';
 
   constructor(private http: HttpClient) {}
 
-  getPersonalDataset(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getUserData(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}user_data/`);
   }
-
-
-  // getPersonalData(id: number): Observable<PersonalData> {
-  //   return this.http.get<PersonalData>(`${this.apiUrl}/api/personaldataset/${id}/`);
-  // }
-
-  // createPerData(perData: PersonalData): Observable<PersonalData> {
-  //   return this.http.post<PersonalData>(`${this.apiUrl}/api/companies/`, perData);
-  // }
-
-  // updatePersonalData(id: number, perData: PersonalData): Observable<PersonalData> {
-  //   return this.http.put<PersonalData>(`${this.apiUrl}/api/companies/${id}/`, perData)
-  // }
 }
